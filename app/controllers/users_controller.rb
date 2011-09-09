714d7aa1c7f66b9417dba5c432764fd5db7146ca
Robert Whittaker <cyclistsdismount@gmail.com>
2011-09-09 12:28:00 +0100

    A form to sign up new users.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,5 +7,10 @@ class UsersController < ApplicationController
   def new
     @title = 'Sign up'
   end
+  
+  def self.authenticate(email, submitted_password)
+    user = find_by_email(email)
+    user && user.has_pasword?(submitted_password) ? user : nil
+  end
 
 end
