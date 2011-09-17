d04c79271b5b3344bbebb01312c72b054674a352
Robert Whittaker <cyclistsdismount@gmail.com>
2011-09-17 19:01:31 +0100

    Testing signup failure with lambda.

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -4,17 +4,19 @@ describe "Users" do
   describe "signup" do
     describe "failure" do
       it "should not make a new user" do
-        visit signup_path
+        lambda do
+          visit signup_path
         
-        fill_in "Name",         :with => ""
-        fill_in "Email",        :with => ""
-        fill_in "Password",     :with => ""
-        fill_in "Confirmation", :with => ""
+          fill_in "Name",         :with => ""
+          fill_in "Email",        :with => ""
+          fill_in "Password",     :with => ""
+          fill_in "Confirmation", :with => ""
         
-        click_button
+          click_button
         
-        response.should render_template("users/new")
-        response.should have_selector("div#error_explanation")
+          response.should render_template("users/new")
+          response.should have_selector("div#error_explanation")
+        end
       end
     end
   end
