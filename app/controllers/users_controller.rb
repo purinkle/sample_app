6c4f4f08c9ef11263427791d37963b086e103278
Robert Whittaker <cyclistsdismount@gmail.com>
2011-09-17 18:14:59 +0100

    The user create action with a save and a redirect.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,7 +13,7 @@ class UsersController < ApplicationController
     @user = User.new( params[:user] )
     
     if @user.save
-      # Handle a successful save.
+      redirect_to @user
     else
       @title = 'Sign up'
 
