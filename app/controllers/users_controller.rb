9b70175fecdb45690cb6d6d358aea701f57e0a09
Robert Whittaker <cyclistsdismount@gmail.com>
2011-09-19 12:27:12 +0100

    Added code to sign in users upon signup.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,13 +13,13 @@ class UsersController < ApplicationController
     @user = User.new( params[:user] )
     
     if @user.save
-      flash[:success] = "Welcome to the Sample App!"
+      sign_in @user
+
+      flash[:success] = 'Welcome to the Sample App!'
+
       redirect_to @user
     else
       @title = 'Sign up'
-      
-      @user.password = ""
-      @user.password_confirmation = ""
 
       render 'new'
     end
