18108ac18e2daaa00583570923a59db08cdeaa67
Robert Whittaker <robert.whittaker@onthebeach.co.uk>
2011-10-21 10:54:36 +0100

    Added tests to make usre delete links only appear for admin users.

--- spec/requests/layout_links_spec.rb
+++ spec/requests/layout_links_spec.rb
@@ -1,32 +1,32 @@
 require 'spec_helper'
 
 describe "LayoutLinks" do
-  
+
   it "should have a Home page at '/'" do
     get '/'
     response.should have_selector('title', :content => "Home")
   end
-  
+
   it "should have a Contact page at '/contact'" do
     get '/contact'
     response.should have_selector('title', :content => "Contact")
   end
-  
+
   it "should have an About page at '/about'" do
     get '/about'
     response.should have_selector('title', :content => "About")
   end
-  
+
   it "should have a Help page at '/help'" do
     get '/help'
     response.should have_selector('title', :content => "Help")
   end
-  
+
   it "should have a Sign Up page at '/signup'" do
     get 'signup'
     response.should have_selector('title', :content => "Sign up")
   end
-  
+
   it "should have the right links on the layout" do
     visit root_path
     click_link "About"
@@ -40,11 +40,11 @@ describe "LayoutLinks" do
     click_link "Sign up now!"
     response.should have_selector('title', :content => "Sign up")
   end
-  
+
   describe 'when not signed in' do
     it 'should have a signin link' do
       visit root_path
-      
+
       response.should have_selector(
         'a',
           :href    => signin_path,
@@ -52,37 +52,58 @@ describe "LayoutLinks" do
       )
     end
   end
-  
+
   describe 'when signed in' do
     before (:each) do
       @user = Factory(:user)
-      
+
       visit signin_path
-      
+
       fill_in :email,    :with => @user.email
       fill_in :password, :with => @user.password
-      
+
       click_button
     end
-    
+
     it 'should have a signout link' do
       visit root_path
-      
+
       response.should have_selector(
         'a',
           :href    => signout_path,
           :content => 'Sign out'
       )
     end
-    
+
     it 'should have a profile link' do
       visit root_path
-      
+
       response.should have_selector(
         'a',
           :href    => user_path(@user),
           :content => 'Profile'
       )
+    end
+
+    describe "as a non-admin user" do
+      it "should not have a delete link" do
+        visit users_path
+        response.should_not have_selector("a", :href => user_path(@user),
+                                           :content => "Delete #{@user.name}")
+      end
+    end
+
+    describe "as an admin user" do
+      it "should have a delete link" do
+        @user = Factory(:user, :email => "admin@example.com", :admin => true)
+        visit signin_path
+        fill_in :email,    :with => @user.email
+        fill_in :password, :with => @user.password
+        click_button
+        visit users_path
+        response.should have_selector("a", :href => user_path(@user),
+                                           :content => "delete")
+      end
     end
   end
 end
