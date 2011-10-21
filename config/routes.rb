289a107dad333d59c09a6ba41a1ab9c3c7b9d473
Robert Whittaker <cyclistsdismount@gmail.com>
2011-10-21 16:15:11 +0100

    Created destroy and create routes for microposts.

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,9 @@ SampleApp::Application.routes.draw do
   get "sessions/new"
 
   resources :users
-  resources :sessions, :only => [:new, :create, :destroy]
-  
+  resources :sessions,   :only => [:new, :create, :destroy]
+  resources :microposts, :only => [:create, :destroy]
+
   match '/signup',  :to => 'users#new'
   match "/signin",  :to => "sessions#new"
   match "/signout", :to => "sessions#destroy"
@@ -11,7 +12,7 @@ SampleApp::Application.routes.draw do
   match '/contact', :to => 'pages#contact'
   match '/about',   :to => 'pages#about'
   match '/help',    :to => 'pages#help'
-  
+
   root :to => 'pages#home'
 
   # The priority is based upon order of creation:
